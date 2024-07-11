1f31a5a8617fb3cce65bcebde1e2e38a814d6cd4
Interaktika Studio <dazedigital@gmail.com>
2024-07-11 06:14:27 -0400

    REACT_APP_API_BASE_URL revised and console error check

--- client/src/index.js
+++ client/src/index.js
@@ -7,6 +7,8 @@ import App from './App';
 
 const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
 console.log('API_BASE_URL:', API_BASE_URL);
+console.log('REACT_APP_API_BASE_URL:', process.env.REACT_APP_API_BASE_URL);
+
 
 const axiosInstance = axios.create({
   baseURL: API_BASE_URL, // Corrected baseURL to API_BASE_URL
@@ -18,7 +20,11 @@ const axiosInstance = axios.create({
 });
 
 console.log('API_BASE_URL:', API_BASE_URL);
+console.log('REACT_APP_API_BASE_URL:', process.env.REACT_APP_API_BASE_URL);
+
 console.log('axiosInstance:', axiosInstance);
+
+console.log('Source path of .env file:', __filename); // Log the source path of the file loaded
 
 const root = createRoot(document.getElementById('root'));
 
