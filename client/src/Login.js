4e3fd14333b425d4c6de6ac0558e0a2da91e7de9
Interaktika Studio <dazedigital@gmail.com>
2024-07-12 09:15:18 -0400

    env changes inside login

--- client/src/Login.js
+++ client/src/Login.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useNavigate, Link } from 'react-router-dom';
 import { useAuth } from './AuthContext';
 import axiosInstance from '../src/API/axiosInstance'; // Import axiosInstance
@@ -16,12 +16,17 @@ const Login = () => {
     const [message, setMessage] = useState('');
     const [loading, setLoading] = useState(false);
 
+    useEffect(() => {
+        console.log('Using environment file:', process.env.NODE_ENV);
+        console.log('API_BASE_URL:', process.env.REACT_APP_API_BASE_URL);
+    }, []);
+
     const handleEmailChange = (e) => {
         const enteredEmail = e.target.value;
         setEmail(enteredEmail);
         setShowNextButton(enteredEmail.includes('@'));
     };
-    
+
     const handleNext = () => {
         setEmailReadOnly(true);
     };
