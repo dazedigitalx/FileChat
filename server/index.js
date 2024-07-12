4326a973fd0605c8403bbe973e23c00ad33d4216
Interaktika Studio <dazedigital@gmail.com>
2024-07-12 09:35:20 -0400

    serverside index.js cors updated

--- server/index.js
+++ server/index.js
@@ -27,15 +27,13 @@ app.use(express.json());
 const allowedOrigins = (process.env.CORS_ALLOWED_ORIGINS || '').split(',');
 
 app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  credentials: true
+  origin: allowedOrigins.length ? allowedOrigins : '*', // Allow all if no origins specified
+  credentials: true,
 }));
+
+
+app.options('*', cors()); // Handle preflight requests
+  
 
 // Connect to MongoDB
 connectDB()
