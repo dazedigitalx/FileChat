f26b73cc26159c59bc98d07104a7511329836649
Interaktika Studio <dazedigital@gmail.com>
2024-07-14 12:45:58 -0400

    CORS update

--- server/index.js
+++ server/index.js
@@ -7,21 +7,22 @@ const helmet = require('helmet');
 const guestChannelRoutes = require('./routes/guestChannelRoutes.js');
 const channelRouter = require('./routes/channelRouter');
 
-
 dotenv.config();
 
 const app = express();
 
+// CORS configuration
+const corsOptions = {
+  origin: ['http://localhost:3000', 'https://file-chat-client.vercel.app'], // Allow multiple origins
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true, // Allow cookies to be sent
+};
+
 // Middleware setup
-app.use(cors());
+app.use(cors(corsOptions)); // Apply CORS middleware with options
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(helmet()); // Optional: adds security headers
-
-
-app.use(cors({
-  origin: 'http://localhost:3000' // Allow requests from this origin
-}));
 
 // Connect to MongoDB
 connectDB()
@@ -31,13 +32,10 @@ connectDB()
     process.exit(1);
   });
 
-// Mount 
-
-// Other routes
+// Mount routes
 app.use('/api/anonymous', guestChannelRoutes);
-
 app.use('/api/users', require('./routes/userRouter'));
-app.use('/api/channels', require('./routes/channelRouter'));
+app.use('/api/channels', channelRouter);
 app.use('/api/messages', require('./routes/messageRouter'));
 
 // Error handling middleware
